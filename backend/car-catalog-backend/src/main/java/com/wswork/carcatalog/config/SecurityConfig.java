a8537f480727e240b6ae26b46baab5016fca189d
Leonardostavares <leosilvatavares13@gmail.com>
2025-08-17 23:11:20 -0300

    Correção CORS: priorizar URL correta do frontend

--- backend/car-catalog-backend/src/main/java/com/wswork/carcatalog/config/SecurityConfig.java
+++ backend/car-catalog-backend/src/main/java/com/wswork/carcatalog/config/SecurityConfig.java
@@ -34,8 +34,8 @@ public class SecurityConfig {
         
         // URLs permitidas
         List<String> allowedOrigins = Arrays.asList(
-            "https://car-frontend-leo.herokuapp.com",
             "https://car-frontend-leo-cbf29182969f.herokuapp.com",
+            "https://car-frontend-leo.herokuapp.com",
             "https://car-catalog-frontend-leonardo-ee72ebc72d08.herokuapp.com",
             "https://car-catalog-frontend-leonardo.herokuapp.com",
             "http://localhost:3000",
